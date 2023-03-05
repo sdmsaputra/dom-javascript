6c90e9abc169dc1937b5834ac3b359eea7fbfea0
Sadam Saputra <sadampbgarena2@gmail.com>
2023-03-05 20:07:41 +0700

    Mempelajari DOM Selection

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-const nama = 'Nama'
+// DOM SELECTION
+// document.getElementById(judul)
+const judul = document.getElementById('judul');
+judul.style.color = 'blue';
+judul.style.backgroundColor = 'yellow';
+judul.innerHTML = 'Form Style JavaScript';
+
+// document.getElementsByTagName();
+// HTMLCollection
+const paragraph = document.getElementsByTagName('p');
+for(let i = 0; i < paragraph.length; i++) {
+    paragraph[i].style.color = 'blue';    
+}
+
+// document.getElementsByClassName();
+// HTMLCollection
+const p1 = document.getElementsByClassName('p1')[0];
+p1.innerHTML = 'Form Style';
+
+
+// document.querySelectorAll();

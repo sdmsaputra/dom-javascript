7d06915cf6e25ffdb0f6edb6133593ce5c5ea8c1
Sadam Saputra <sadampbgarena2@gmail.com>
2023-03-06 17:52:33 +0700

    Menambahkan addEventListener

--- event.js
+++ event.js
@@ -1,7 +1,34 @@
-const p3 = document.querySelector('.p3');
-function changeColor() {
-    p2.style.backgroundColor = 'lightblue';
-}
+// const p2 = document.querySelector('.p2');
+// function changeColorP2() {
+//     p2.style.backgroundColor = 'lightblue';
+// }
 
-const p2 = document.querySelector('.p2');
-p2.onClick = changeColor;
+// const p3 = document.querySelector('.p3');
+// function changeColorP3() {
+//     p3.style.backgroundColor = 'lightblue';
+// }
+// p2.onclick = changeColorP2;
+// p3.onclick = changeColorP3;
+
+// // Menambahkan Item List
+// const p4 = document.querySelector('section#b p');
+// p4.addEventListener('click', function() {
+//     const ul = document.querySelector('section#b ul');
+//     const newLi = document.createElement('li');
+//     const textLi = document.createTextNode('List Item Baru nih');
+//     newLi.appendChild(textLi);
+//     ul.appendChild(newLi);
+// });
+
+const p3 = document.querySelector('.p3');
+// p3.onclick = function() {
+//     p3.style.backgroundColor = 'lightblue';
+// };
+
+p3.addEventListener('click', function() {
+    p3.style.backgroundColor = 'lightblue';
+});
+
+p3.addEventListener('click', function() {
+    p3.style.color = 'red';
+});
--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-// DOM SELECTION
-// document.getElementById(judul)
-// const judul = document.getElementById('judul');
-// judul.style.color = 'blue';
-// judul.style.backgroundColor = 'yellow';
-// judul.innerHTML = 'Form Style JavaScript';
+DOM SELECTION
+document.getElementById(judul)
+const judul = document.getElementById('judul');
+judul.style.color = 'blue';
+judul.style.backgroundColor = 'yellow';
+judul.innerHTML = 'Form Style JavaScript';
 
-// // document.getElementsByTagName();
-// // HTMLCollection
-// const paragraph = document.getElementsByTagName('p');
-// for(let i = 0; i < paragraph.length; i++) {
-//     paragraph[i].style.color = 'blue';    
-// }
+// document.getElementsByTagName();
+// HTMLCollection
+const paragraph = document.getElementsByTagName('p');
+for(let i = 0; i < paragraph.length; i++) {
+    paragraph[i].style.color = 'blue';    
+}
 
-// // document.getElementsByClassName();
-// // HTMLCollection
-// const p1 = document.getElementsByClassName('p1')[0];
-// p1.innerHTML = 'Form Style';
+// document.getElementsByClassName();
+// HTMLCollection
+const p1 = document.getElementsByClassName('p1')[0];
+p1.innerHTML = 'Form Style';
 
 
-// // document.querySelectorAll(); - Menanyakan mengenai selector (CSS selector) - Menghasilkan ELement
-// const p4 = document.querySelector('#b p');
-// p4.style.color = 'blue';
-// p4.style.backgroundColor = 'yellow';
+// document.querySelectorAll(); - Menanyakan mengenai selector (CSS selector) - Menghasilkan ELement
+const p4 = document.querySelector('#b p');
+p4.style.color = 'blue';
+p4.style.backgroundColor = 'yellow';
 
 
